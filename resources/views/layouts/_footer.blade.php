282409cf3d0bd855cdb97c67d02e94006f2abdf9
sunxiaofeng123 <18703137630@163.com>
2018-07-13 22:00:11 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://blog.welcomegohome.com" target="_blank">雁过留声</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
