132c986adfd9f5930d57b91a92ba1ee76e2ff686
sunxiaofeng123 <18703137630@163.com>
2018-07-18 22:49:04 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,18 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count',
+                            'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //关联分类表一对一关系
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //关联用户表一对一关系
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
