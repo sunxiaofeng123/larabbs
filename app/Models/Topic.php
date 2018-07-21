4935865c463278f5d7fc86841126cefb7ede592f
sunxiaofeng123 <18703137630@163.com>
2018-07-21 14:15:04 +0800

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -18,4 +18,39 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * @param $query
+     * @param $order
+     * @return 查询构建器，关于排序, 本地作用域,调用时不用加scope前缀
+     */
+    public function scopeWithOrder($query, $order)
+    {
+        //不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        //防止N+1
+        return $query->with('user', 'category');
+    }
+
+    //最新回复
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    //最新发布
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
+
+
 }
